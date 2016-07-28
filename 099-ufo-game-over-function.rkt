bec5868ead188615930a15dec41fa9d28144401a
eareese <eareese@eareese.com>
2016-07-28 14:51:21 -0400

    Update exercise 99

--- 099-ufo-game-over-function.rkt
+++ 099-ufo-game-over-function.rkt
@@ -2,23 +2,15 @@
 (require 2htdp/image)
 (require 2htdp/universe)
 
-;; Design a game program using the 2htdp/universe library for playing a simple
-;; space invader game. The player is in control of a tank (a small rectangle)
-;; that must defend our planet (the bottom of the canvas) from a UFO (see
-;; Intervals for one possibility) that descends from the top of the canvas to
-;; the bottom. In order to stop the UFO from landing, the player may fire a
-;; single missile (a triangle smaller than the tank) by hitting the space bar.
-;; In response, the missile emerges from the tank. If the UFO collides with the
-;; missile, the player wins; otherwise the UFO lands and the player loses.
-
-;; Here are some details concerning the three game objects and their movements.
-;; First, the tank moves a constant speed along the bottom of the canvas though
-;; the player may use the left arrow key and the right arrow key to change
-;; directions. Second, the UFO descends at a constant velocity but makes small
-;; random jumps to the left or right. Third, once fired the missile ascends
-;; along a straight vertical line at a constant speed at least twice as fast as
-;; the UFO descends. Finally, the UFO and the missile collide if their reference
-;; points are close enough, for whatever you think “close enough” means.
+;; Exercise 99. Design the function si-game-over? for use as the stop-when
+;; handler. The game stops if the UFO lands or if the missile hits the UFO. For
+;; both conditions, we recommend that you check for proximity of one object to
+;; another.
+;
+;; The stop-when clause allows for an optional second sub-expression, namely a
+;; function that renders the final state of the game. Design si-render-final and
+;; use it as the second part for your stop-when clause in the main function of
+;; exercise 101.
 
 
 ;; Structure type definitions
@@ -60,6 +52,7 @@
 
 (define TANK (rectangle 30 15 "solid" "blue"))
 (define TANK-Y (- (image-height BACKGROUND) (* 3/5 (image-height TANK))))
+(define TANK-DELTA-X 3)
 
 (define MISSILE (triangle 7 "solid" "red"))
 
@@ -67,52 +60,80 @@
   (overlay (circle 4 "solid" "green")
            (rectangle 20 2 "solid" "green")))
 
+(define REACH 5)
 
-; Tank Image -> Image
-; adds t to the given image img
-(check-expect (tank-render (make-tank 20 3) BACKGROUND)
-              (place-image TANK 20 TANK-Y BACKGROUND))
-(define (tank-render t img)
-  (place-image TANK (tank-loc t) TANK-Y img))
 
-; UFO Image -> Image
-; adds u to the given image img
-(check-expect (ufo-render (make-posn 66 33) BACKGROUND)
-              (place-image UFO 66 33 BACKGROUND))
-(define (ufo-render u img)
-  (place-image UFO (posn-x u) (posn-y u) img))
+; in-reach?
+;; Posn Posn -> Boolean
+; determines whether a given Posn is "close enough" to another given Posn, that
+;; is, if the distance between Posns is strictly less than some constant REACH.
+(check-expect
+ (in-reach? (make-posn 50 50)
+            (make-posn 50 (+ 50 (* .5 REACH))))
+ #t)
+(check-expect
+ (in-reach? (make-posn 50 50) (make-posn (- 50 (* .5 REACH))
+                                         (+ 50 (* .5 REACH))))
+ #t)
+(check-expect
+ (in-reach? (make-posn 50 50)
+            (make-posn 50 50))
+ #t)
+(check-expect
+ (in-reach? (make-posn 50 50)
+            (make-posn (+ 50 (+ 10 REACH)) (+ 50 (+ 10 REACH))))
+ #f)
+(check-expect (in-reach? (make-posn 50 50) (make-posn 50 50)) #t)
+(define (in-reach? p1 p2)
+   (<= (disty p1 p2) REACH))
 
-; Missile Image -> Image
-; adds m to the given image img
-(check-expect (missile-render (make-posn 100 100) BACKGROUND)
-              (place-image MISSILE 100 100 BACKGROUND))
-(define (missile-render m img)
-  (place-image MISSILE (posn-x m) (posn-y m) img))
-
-; SIGS -> Image
-; adds TANK, UFO, and possibly the MISSILE to BACKGROUND
-(define (si-render s)
-  (cond
-    [(aim? s)
-     (tank-render (aim-tank s)
-                  (ufo-render (aim-ufo s) BACKGROUND))]
-    [(fired? s)
-     (tank-render (fired-tank s)
-                  (ufo-render (fired-ufo s)
-                              (missile-render (fired-missile s) BACKGROUND)))]))
+;; distance formula by itself
+; Posn Posn -> Number
+; calculate the distance between two points
+(check-expect (disty (make-posn 0 5) (make-posn 0 0))
+              5)
+(check-expect (disty (make-posn 5 0) (make-posn 0 0))
+              5)
+(check-expect (disty (make-posn 3 0) (make-posn 0 4))
+              5)
+(check-expect (disty (make-posn -5 -5) (make-posn -5 -5))
+              0)
+(define (disty p1 p2)
+  (sqrt (+ (sqr (- (posn-x p2) (posn-x p1))) (sqr (- (posn-y p2) (posn-y p1))))))
 
 
 ; SIGS -> Boolean
 ; si-game-over?
 ; stop the game if the UFO lands or if the missile hits the ufo
 ; land condition: ufo's y becomes scene height
-; missile cond: and (missile x is close to ufo x) (missle y close to ufo y)
-; what is close to? something to do with their width/size?
+; missile cond: use in-reach? from Exercise 97
+(check-expect ;; in-reach? should be #t
+ (si-game-over
+  (make-fired (make-posn 75 100)
+              (make-tank 50 TANK-DELTA-X)
+              (make-posn (+ 75 (* .5 REACH))
+                         (+ 100 (* .5 REACH))))) #t)
+(check-expect ;; in-reach? should be #f
+ (si-game-over
+  (make-fired (make-posn 75 100)
+              (make-tank 50 TANK-DELTA-X)
+              (make-posn 5 5))) #f)
+(check-expect ;; TODO: check land condition
+ (si-game-over
+  (make-aim (make-posn 75 100)
+            (make-tank 50 TANK-DELTA-X))) #f)
+(define (si-game-over w)
+  (cond [(>= (posn-y (if (aim? w) (aim-ufo w) (fired-ufo w))) (image-height BACKGROUND)) #t]
+        [(and (fired? w) (in-reach? (fired-ufo w) (fired-missile w))) #t]
+        [else #f]))
 
+;; si-render-image
+; SIGS -> Image
+(check-expect (si-render-final
+               (make-aim (make-posn 20 10) (make-tank 28 -3)))
+              (overlay
+               (text "GAME OVER" 24 "red") BACKGROUND))
+(define (si-render-final w)
+  (overlay (text "GAME OVER" 24 "red") BACKGROUND))
 
-;; INITIAL SCENE
-(place-images
- (list UFO TANK)
- (list (make-posn (* 1/2 (image-width BACKGROUND)) (image-height UFO))
-       (make-posn (* 1/2 (image-width BACKGROUND)) TANK-Y))
- BACKGROUND)
+(si-render-final (make-aim (make-posn 20 10) (make-tank 28 -3)))
